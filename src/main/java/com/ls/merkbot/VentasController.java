ec9e8c9a36a4fa5a91a2e584f2996fe80e83ced0
GuilleJozu <89705180+GuilleJozu@users.noreply.github.com>
2025-11-22 01:37:45 -0600

    Update VentasController.java

--- src/main/java/com/ls/merkbot/VentasController.java
+++ src/main/java/com/ls/merkbot/VentasController.java
@@ -15,6 +15,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.io.InputStream;
+import java.io.FileNotFoundException;
 
 @Controller
 @RequestMapping("/ventas")
