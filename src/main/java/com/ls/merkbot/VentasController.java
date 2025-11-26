0bacb75e89d8d18d033af80cad8b112510f9377f
GuilleJozu <89705180+GuilleJozu@users.noreply.github.com>
2025-11-25 19:18:30 -0600

    Update VentasController.java

--- src/main/java/com/ls/merkbot/VentasController.java
+++ src/main/java/com/ls/merkbot/VentasController.java
@@ -1,5 +1,6 @@
 package com.ls.merkbot;
 
+import java.net.URL;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
